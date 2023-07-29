066b9f12951244cad39498f74e27174d7fe0f533
KUNAPAREDDY SUDHA RANI <126180396+Sudharani3@users.noreply.github.com>
2023-07-29 15:28:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import Greet from './Components/Greet';
+// import Welcome from './Components/Welcome';
+// import Message from './Components/Message';
+// function App() {
+//   return (
+//     <div className="App">
+//      {/* function call */}
+//      {/* <Greet name="Warner" firstname="David"/> 
+//      <Greet name="Smith" firstname="Steve"/> 
+//      <Greet name="Brook" firstname="Harry"/> 
+//      <Welcome name="Rani"/> */}
+//      <Message />
+//     </div>
+//   );
+// }
+
+// export default App;
+import logo from './logo.svg';
+import Calc from './Calculator/Calc';
+function App() {
+    return (
+      <div className="App">
+     <Calc/>
+   </div>
+    );
+  }
+  
+  export default App;
